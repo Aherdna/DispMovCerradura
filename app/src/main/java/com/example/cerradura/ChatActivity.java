5a76eb2eba8458ebb1e11eee4e42a7b8bac639f4
Andrea Hernández <andreahdze@hotmail.com>
2021-09-22 22:42:25 -0500

    ChatActivity, card_view_mensajes

---
+++ app/src/main/java/com/example/cerradura/ChatActivity.java
@@ -0,0 +1,32 @@
+package com.example.cerradura;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ChatActivity extends AppCompatActivity {
+
+    private CircleImageView imgPerfil;
+    private TextView nombre;
+    private RecyclerView rvMensajes;
+    private EditText txtMensaje;
+    private Button btnEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        imgPerfil = (CircleImageView)  findViewById(R.id.imgPerfil);
+        nombre = (TextView) findViewById(R.id.nombre);
+        rvMensajes = (RecyclerView) findViewById(R.id.rvMensajes);
+        txtMensaje = (EditText) findViewById(R.id.txtMensaje);
+        btnEnviar = (Button) findViewById(R.id.btnEnviar);
+    }
+}
